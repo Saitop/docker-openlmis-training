11c04d7cf9acce3c5a5f6c214d41eef1e4708cf5
pfcui <cuipengfei2008@gmail.com>
2016-11-02 17:26:03 +0800

    locale

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,5 @@
+export JAVA_OPTS="-DdefaultLocale=en"
+
 export CLASSPATH=$CLASSPATH:/usr/local/tomcat/webapps/extra/properties
 #this file will to copied to the bin dir of tomcat
 #it will be ran each time tomcat starts up, so that the properties files are picked up
