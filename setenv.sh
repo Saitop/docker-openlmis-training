17136a6a16eb628b1fa0c5a2248fb1dbf29c19ab
pfcui <cuipengfei2008@gmail.com>
2016-11-02 16:51:30 +0800

    enable properties files

---
+++ setenv.sh
@@ -0,0 +1,3 @@
+export CLASSPATH=$CLASSPATH:/usr/local/tomcat/webapps/openlmis-web/properties
+#this file will to copied to the bin dir of tomcat
+#it will be ran each time tomcat starts up, so that the properties files are picked up
