803ba60be2138e8d32807f0e5e5cd3fbb2c381c0
pfcui <cuipengfei2008@gmail.com>
2016-11-04 15:41:09 +0800

    path

--- run_time/start.sh
+++ run_time/start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#complete properties files and ini file with run time input env vars
-ruby replace_configs.rb
+#replace placeholders in properties files and ini file with run time input env vars
+ruby /run_time/replace_configs.rb
+
+#start cubes
+sh /app/cubes/bin/start.sh
 
 #setenv.sh will make sure tomcat pickup the extra properties files
 mv /run_time/setenv.sh /usr/local/tomcat/bin/setenv.sh
 #start tomcat
 catalina.sh run
-
-#start cubes
-sh /app/cubes/bin/start.sh
