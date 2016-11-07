d179f68caffa311b4626f938f6bb0ba13f9a2e3c
pfcui <cuipengfei2008@gmail.com>
2016-11-07 11:02:21 +0800

    add template

--- run_time/start.sh
+++ run_time/start.sh
@@ -7,6 +7,6 @@ ruby /run_time/replace_configs.rb
 sh /app/cubes/bin/start.sh
 
 #setenv.sh will make sure tomcat pickup the extra properties files
-mv /run_time/setenv.sh /usr/local/tomcat/bin/setenv.sh
+cp /run_time/setenv.sh /usr/local/tomcat/bin/setenv.sh
 #start tomcat
 catalina.sh run
