33252f8d0edc093f7ca8dc47e3fc85e9be8983f1
pfcui <cuipengfei2008@gmail.com>
2016-11-07 12:07:45 +0800

    path wrong

--- run_time/setenv.sh
+++ run_time/setenv.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 export JAVA_OPTS="-DdefaultLocale=en"
 
-export CLASSPATH=$CLASSPATH:/usr/local/tomcat/webapps/extra/properties
+export CLASSPATH=$CLASSPATH:/usr/local/tomcat/extra_properties
 #this file will to copied to the bin dir of tomcat
 #it will be ran each time tomcat starts up, so that the properties files are picked up
