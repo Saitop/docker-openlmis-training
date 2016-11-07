1e1ce2695f892ec518efd02d9048d3e97d13b2ae
pfcui <cuipengfei2008@gmail.com>
2016-11-07 11:26:55 +0800

    add to bash

--- run_time/replace_configs.rb
+++ run_time/replace_configs.rb
@@ -1,1 +1,7 @@
-puts "nothing here yet"
+pg_credentials=["POSTGRES_USER_NAME",
+                "POSTGRES_PASSWORD",
+                "POSTGRES_HOST"].map { |key| key+"="+ENV[key] }
+
+File.open("/root/.bashrc", 'w') { |file| file.write(pg_credentials.join("\n")) }
+# code above adds pg credentials to bashrc so the start script of cubes will pick them up and replace them
+
