72d4b984ebbd073d18f11eb64a2b0655071072ed
pfcui <cuipengfei2008@gmail.com>
2016-11-08 17:16:42 +0800

    order

--- run_time/start.sh
+++ run_time/start.sh
@@ -4,11 +4,11 @@
 #using the env vars provided with "docker run"
 ruby /run_time/replace_configs.rb
 
+#run migration
+/run_time/migration.sh
+
 #start cubes
 sh /app/cubes/bin/start.sh
-
-#run migration before start tomcat
-/run_time/migration.sh
 
 #setenv.sh will make sure tomcat pickup the extra properties files
 cp /run_time/setenv.sh /usr/local/tomcat/bin/setenv.sh
